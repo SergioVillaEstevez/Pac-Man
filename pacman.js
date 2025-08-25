36fd36a2d1bb75c3fd8bc559484c07a0820a1dec
SergioVillaEstevez <sergiovillaestevez@gmail.com>
2025-08-25 18:19:00 +0200

    red ghost

--- pacman.js
+++ pacman.js
@@ -4,46 +4,50 @@ const ctx = canvas.getContext("2d")
 
 const mapa = [
   [1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,0,2,2,2,2,1,0,0,2,2,2,1],
-  [1,2,0,1,0,1,1,1,0,2,2,2,1],
-  [1,2,0,0,0,0,1,2,0,2,2,2,1],
-  [1,0,0,0,0,0,0,2,0,2,2,2,1],
-  [1,2,0,0,1,3,0,2,1,2,2,2,1],
-  [1,1,1,0,1,1,0,1,1,2,1,1,1],
-  [0,0,1,0,1,0,0,2,1,2,1,0,0],
-  [1,1,1,0,0,0,0,2,0,2,1,1,1],
-  [0,2,0,0,1,0,0,2,1,2,0,0,0],
-  [1,1,1,0,0,0,0,2,0,2,1,1,1],
-  [0,0,1,0,0,0,1,0,0,2,1,0,0],
-  [1,1,1,0,0,1,1,1,0,2,1,1,1],
-  [1,2,2,2,0,2,2,2,0,2,1,2,1],
-  [1,2,2,2,0,2,2,2,0,2,0,2,1],
-  [1,2,2,2,0,2,2,2,0,2,1,2,1],
-  [1,2,2,2,0,2,2,2,0,0,1,2,1],
-  [1,2,2,2,0,2,2,2,0,0,1,2,1],
-  [1,0,0,0,0,2,2,2,0,0,0,0,1],
+  [1,2,2,2,2,2,1,2,2,2,2,2,1],
+  [1,2,2,1,2,1,1,1,2,2,2,2,1],
+  [1,2,2,2,2,2,1,2,2,2,2,2,1],
+  [1,2,2,2,2,2,2,2,2,2,2,2,1],
+  [1,2,2,2,1,1,2,2,1,2,2,2,1],
+  [1,1,1,2,1,1,2,1,1,2,1,1,1],
+  [0,0,1,2,1,2,2,2,1,2,1,0,0],
+  [1,1,1,2,2,2,2,2,2,2,1,1,1],
+  [1,2,2,2,1,2,2,2,1,2,2,2,2],
+  [1,1,1,2,2,2,2,2,2,2,1,1,1],
+  [0,0,1,2,2,2,1,2,2,2,1,0,0],
+  [1,1,1,2,2,1,1,1,2,2,1,1,1],
+  [1,2,2,2,2,2,2,2,2,2,2,2,1],
+  [1,2,2,2,2,2,4,2,2,2,2,2,1],
+  [1,2,2,2,1,2,1,2,2,2,1,2,1],
+  [1,2,2,2,1,2,1,2,2,2,1,2,1],
+  [1,2,2,2,2,2,2,2,2,2,1,2,1],
+  [1,2,1,2,2,2,1,2,2,2,2,2,1],
   [1,1,1,1,1,1,1,1,1,1,1,1,1],
 
 ];
 
 let titleSize= 30
 
+let pacmanX=1;
+let pacmanY=9;
+let dx= 1;
+let dy= 0;
 
 const drawMap = ()=> {
-
-
+    
+    
     ctx.clearRect(0,0,canvas.width, canvas.height);
-
+    
     for(let y= 0; y< mapa.length; y++ ){
 
         for(let x= 0;x< mapa[y].length;x++ ){
 
             const celda = mapa[y][x]
-
+            
             if(celda===1){
-                ctx.fillStyle="blue"
+                ctx.fillStyle= "blue"
                 ctx.fillRect(x*titleSize, y*titleSize, titleSize,titleSize)
-                ctx.fill()
+                
             }
             else if(celda===2){
                 ctx.fillStyle="white"
@@ -57,7 +61,7 @@ const drawMap = ()=> {
                 );
                 ctx.fill()
             }
-
+            
             else if(celda === 3){
 
                 ctx.fillStyle= "yellow"
@@ -66,113 +70,147 @@ const drawMap = ()=> {
                 ctx.fill()
 
             }
-
-
-
-
+            else if(celda === 4){
+                ctx.fillStyle = "red"
+                ctx.beginPath()
+                ctx.arc(x*titleSize + titleSize/2,y*titleSize + titleSize/2, 8,0,Math.PI*2)
+                ctx.fill()
+            }
+            
+            
+            
+            
         }
     }
-
+    
 }
 
 function moverPacmanDerecha() {
-  for (let y = 0; y < mapa.length; y++) {
-    for (let x = 0; x < mapa[y].length; x++) {
-      if (mapa[y][x] === 3) { // encontramos a Pac-Man
-        const nuevaX = x + 1;
-        const nuevaY = y;
-
+    for (let y = 0; y < mapa.length; y++) {
+        for (let x = 0; x < mapa[y].length; x++) {
+            if (mapa[y][x] === 3) { // encontramos a Pac-Man
+                const nuevaX = x + 1;
+                const nuevaY = y;
+                
         // comprobar si no es muro
         if (mapa[nuevaY][nuevaX] !== 1) {
-          mapa[y][x] = 0;           // vaciar celda actual
-          mapa[nuevaY][nuevaX] = 3; // poner a Pac-Man en nueva celda
+            mapa[y][x] = 0;           // vaciar celda actual
+            mapa[nuevaY][nuevaX] = 3; // poner a Pac-Man en nueva celda
         }
-
+        
         return; // salir porque ya lo movimos
-      }
     }
-  }
+}
+}
 }
 
 function moverPacmanArriba() {
-  for (let y = 0; y < mapa.length; y++) {
-    for (let x = 0; x < mapa[y].length; x++) {
-      if (mapa[y][x] === 3) { // encontramos a Pac-Man
-        const nuevaX = x;
-        const nuevaY = y-1;
-
-        // comprobar si no es muro
-        if (mapa[nuevaY][nuevaX] !== 1) {
-          mapa[y][x] = 0;           // vaciar celda actual
-          mapa[nuevaY][nuevaX] = 3; // poner a Pac-Man en nueva celda
-        }
-
-        return; // salir porque ya lo movimos
+    for (let y = 0; y < mapa.length; y++) {
+        for (let x = 0; x < mapa[y].length; x++) {
+            if (mapa[y][x] === 3) { // encontramos a Pac-Man
+                const nuevaX = x;
+                const nuevaY = y-1;
+                
+                // comprobar si no es muro
+                if (mapa[nuevaY][nuevaX] !== 1) {
+                    mapa[y][x] = 0;           // vaciar celda actual
+                    mapa[nuevaY][nuevaX] = 3; // poner a Pac-Man en nueva celda
+                }
+                
+                return; // salir porque ya lo movimos
       }
     }
   }
 }
 function moverPacmanAbajo() {
-  for (let y = 0; y < mapa.length; y++) {
-    for (let x = 0; x < mapa[y].length; x++) {
+    for (let y = 0; y < mapa.length; y++) {
+        for (let x = 0; x < mapa[y].length; x++) {
       if (mapa[y][x] === 3) { // encontramos a Pac-Man
         const nuevaX = x;
         const nuevaY = y + 1;
-
+        
         // comprobar si no es muro
         if (mapa[nuevaY][nuevaX] !== 1) {
-          mapa[y][x] = 0;           // vaciar celda actual
-          mapa[nuevaY][nuevaX] = 3; // poner a Pac-Man en nueva celda
+            mapa[y][x] = 0;           // vaciar celda actual
+            mapa[nuevaY][nuevaX] = 3; // poner a Pac-Man en nueva celda
         }
-
+        
         return; // salir porque ya lo movimos
-      }
     }
-  }
+}
+}
 }
 
 function moverPacmanIzquierda() {
-  for (let y = 0; y < mapa.length; y++) {
-    for (let x = 0; x < mapa[y].length; x++) {
+    for (let y = 0; y < mapa.length; y++) {
+        for (let x = 0; x < mapa[y].length; x++) {
       if (mapa[y][x] === 3) { // encontramos a Pac-Man
         const nuevaX = x - 1;
         const nuevaY = y;
-
+        
         // comprobar si no es muro
         if (mapa[nuevaY][nuevaX] !== 1) {
-          mapa[y][x] = 0;           // vaciar celda actual
-          mapa[nuevaY][nuevaX] = 3; // poner a Pac-Man en nueva celda
+            mapa[y][x] = 0;           // vaciar celda actual
+            mapa[nuevaY][nuevaX] = 3; // poner a Pac-Man en nueva celda
         }
 
         return; // salir porque ya lo movimos
-      }
     }
-  }
+}
+}
 }
 
 document.addEventListener("keydown",(e)=>{
+    if(e.key==="d"){ dx = 1; dy = 0; }
+    if(e.key==="a"){ dx = -1; dy = 0; }
+    if(e.key==="w"){ dx = 0; dy = -1; }
+    if(e.key==="s"){ dx = 0; dy = 1; }
+});
 
-    if(e.key==="d"){
-        moverPacmanDerecha();
-    }
-    if(e.key==="a"){
-        moverPacmanIzquierda();
-    }
-    if(e.key==="w"){
-        moverPacmanArriba()
-    }
-    if(e.key==="s"){
-        moverPacmanAbajo()
+const moverSoloPacman=()=>{
+
+    const nuevaX= pacmanX+dx
+    const nuevaY= pacmanY + dy
+
+    if(mapa[nuevaY][nuevaX]!==1){
+        mapa[pacmanY][pacmanX]= 0;
+        pacmanX=nuevaX;
+        pacmanY=nuevaY; 
+        mapa[pacmanY][pacmanX]= 3
+
+
     }
 
 
-})
+
+}
+
+
+// document.addEventListener("keydown",(e)=>{
+
+//     if(e.key==="d"){
+//         moverPacmanDerecha();
+//     }
+//     if(e.key==="a"){
+//         moverPacmanIzquierda();
+//     }
+//     if(e.key==="w"){
+//         moverPacmanArriba()
+//     }
+//     if(e.key==="s"){
+//         moverPacmanAbajo()
+//     }
+
+
+// })
 
 
 
 const gameLoop= ()=> {
     
     drawMap();
-    requestAnimationFrame(gameLoop)
+    moverSoloPacman()
+    setTimeout(gameLoop,300)
 }
-    requestAnimationFrame(gameLoop)
+
+gameLoop()
